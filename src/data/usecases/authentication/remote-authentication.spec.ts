7b54520ccadac3e054c6d9d67a81fa2aa989ae08
Vitor Veras <vitor.moura@fortics.com.br>
2020-12-22 02:24:27 -0300

    refactor: move HttpPostClientSpy to its onw file

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,16 +1,8 @@
-import {HttpPostClient} from '../../protocols/http/http-post-client';
+import {HttpPostClientSpy} from '../../test/mock-http-client';
 import {RemoteAuthentication} from './remote-authentication';
 
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct url', () => {
-    class HttpPostClientSpy implements HttpPostClient {
-      url?: string;
-
-      async post(url: string): Promise<void> {
-        this.url = url;
-        return Promise.resolve();
-      }
-    }
     const url = '/any';
     const httpPostClientSpy = new HttpPostClientSpy();
     const sut = new RemoteAuthentication(url, httpPostClientSpy);
