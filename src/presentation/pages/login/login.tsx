6e0a6923efbf612b024e4eb589f10f11b460568a
Robert Kelly <robertkelly1203@gmail.com>
2021-01-18 02:36:34 -0300

    feat: inital login page layout

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,13 +1,49 @@
-import {Button, Layout, Text} from '@ui-kitten/components';
+import {Button, Icon, Input, Layout, Text} from '@ui-kitten/components';
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, StyleSheet, TouchableWithoutFeedback} from 'react-native';
 
 const Login: React.FC = () => {
   return (
     <SafeAreaView style={styles.safearea}>
-      <Layout style={styles.container}>
-        <Text>Login</Text>
-        <Button>Entrar</Button>
+      <Layout style={styles.container} level="4">
+        <Layout style={styles.logoContainer} level="4" />
+        <Layout style={styles.formContainer}>
+          <Text category="h1">Login</Text>
+          <Text category="s1" appearance="hint" style={styles.subtitle}>
+            Enter your details to continue
+          </Text>
+          <Input
+            accessoryLeft={(props) => <Icon {...props} name="email-outline" />}
+            style={styles.emailInput}
+            label="E-mail"
+            placeholder="Ex: john@doe.com"
+          />
+          <Input
+            accessoryLeft={(props) => <Icon {...props} name="lock-outline" />}
+            accessoryRight={(props) => (
+              <TouchableWithoutFeedback onPress={() => {}}>
+                <Icon {...props} name={true ? 'eye-off' : 'eye'} />
+              </TouchableWithoutFeedback>
+            )}
+            style={styles.passwordInput}
+            label="Password"
+            placeholder="**********"
+            secureTextEntry
+          />
+          <Layout style={styles.buttonsContainer}>
+            <Button>Login</Button>
+            <Text
+              status="danger"
+              category="s2"
+              appearance="hint"
+              style={styles.forgotPassword}>
+              Forgot your password?
+            </Text>
+            <Button style={styles.signupButton} appearance="outline">
+              Sign-up
+            </Button>
+          </Layout>
+        </Layout>
       </Layout>
     </SafeAreaView>
   );
@@ -15,7 +51,20 @@ const Login: React.FC = () => {
 
 const styles = StyleSheet.create({
   safearea: {flex: 1},
-  container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  container: {flex: 1},
+  logoContainer: {flex: 1, width: '100%'},
+  formContainer: {
+    flex: 3,
+    padding: 20,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  subtitle: {marginTop: 8},
+  emailInput: {marginTop: 45},
+  passwordInput: {marginTop: 20},
+  buttonsContainer: {marginTop: 60},
+  forgotPassword: {marginTop: 12, alignSelf: 'center'},
+  signupButton: {marginTop: 45},
 });
 
 export default Login;
