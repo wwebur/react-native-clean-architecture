1cc1e8ff5613c2cd8265b56a5a821279bb6cbe3d
Robert Kelly <robertkelly1203@gmail.com>
2021-01-22 09:36:59 -0300

    test: ensure Login calls validation with correct email

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,16 +1,62 @@
+import {Validation} from '@/presentation/protocols/validation';
 import ApplicationProviderMock from '@/presentation/test/application-provider-mock';
-import {render} from '@testing-library/react-native';
+import {
+  cleanup,
+  fireEvent,
+  render,
+  RenderAPI,
+} from '@testing-library/react-native';
 import {InputProps} from '@ui-kitten/components';
+import faker from 'faker';
+import {FormikErrors} from 'formik';
 import React from 'react';
 import {Login} from '..';
+import {LoginFormValues} from './login';
+
+type SutTypes = {
+  sut: RenderAPI;
+  validationSpy: ValidationSpy;
+};
+
+class ValidationSpy implements Validation {
+  errors: void | object | Promise<FormikErrors<LoginFormValues>>;
+  values: LoginFormValues;
+
+  validate(
+    values: LoginFormValues,
+  ): void | object | Promise<FormikErrors<LoginFormValues>> {
+    this.values = values;
+    return this.errors;
+  }
+}
+
+const makeSut = () => {
+  const validationSpy = new ValidationSpy();
+  const sut = render(
+    <ApplicationProviderMock>
+      <Login validation={validationSpy} />
+    </ApplicationProviderMock>,
+  );
+  return {
+    sut,
+    validationSpy,
+  };
+};
+
+const makeFakePerson = (): LoginFormValues => {
+  return {
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+};
 
 describe('Login Page', () => {
+  afterEach(cleanup);
+
   test('Should start with initial state', () => {
-    const {getByTestId} = render(
-      <ApplicationProviderMock>
-        <Login />
-      </ApplicationProviderMock>,
-    );
+    const {
+      sut: {getByTestId},
+    } = makeSut();
 
     const emailInputProps = getByTestId('email_input').props as InputProps;
     const passwordInputProps = getByTestId('password_input')
@@ -23,5 +69,23 @@ describe('Login Page', () => {
     expect(emailInputProps.caption).toBeUndefined();
     expect(passwordInputProps.caption).toBeUndefined();
     expect(passwordInputProps.secureTextEntry).toBe(true);
+  });
+
+  test('Should call Validation with correct email', () => {
+    const {
+      sut: {getByTestId},
+      validationSpy,
+    } = makeSut();
+    const mockPerson = makeFakePerson();
+
+    const emailInput = getByTestId('email_input');
+
+    fireEvent.changeText(emailInput, mockPerson.email);
+
+    expect(emailInput.props.value).toBe(mockPerson.email);
+    expect(validationSpy.values).toEqual({
+      email: mockPerson.email,
+      password: '',
+    });
   });
 });
