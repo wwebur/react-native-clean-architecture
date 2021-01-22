e8e2fcfc57fd37fca31b99eef58131a222c8f948
Robert Kelly <robertkelly1203@gmail.com>
2021-01-22 01:18:37 -0300

    chore: setup @testing-library/react-native

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,19 @@
 module.exports = {
+  preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
-    '.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|ts|tsx)$': 'babel-jest',
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-navigation|@react-native-community|@ui-kitten|native-base-shoutem-theme)',
+  ],
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
   },
-  //   preset: 'react-native',
-  //   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
