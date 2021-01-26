ae09bbbf25fe2eeef00683986cfab2a2f09db975
Robert Kelly <robertkelly1203@gmail.com>
2021-01-26 13:17:07 -0300

    test: ensure LocalStorageAdapter throws StorageSetError when AsyncStorage throws Error on setItem

--- src/infra/storage/local-storage-adapter.spec.ts
+++ src/infra/storage/local-storage-adapter.spec.ts
@@ -1,4 +1,6 @@
+import {StorageSetError} from '@/domain/errors/storage-set-error';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorageMock from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 import faker from 'faker';
 import {LocalStorageAdapter} from './local-storage-adapter';
 
@@ -14,5 +16,13 @@ describe('LocalStorageAdapter', () => {
     const value = faker.random.word();
     await sut.set(key, value);
     expect(AsyncStorage.setItem).toHaveBeenCalledWith(key, value);
+  });
+  test('Should throw StorageSetError when AsyncStorage throws Error on setItem', async () => {
+    const sut = makeSut();
+    AsyncStorageMock.setItem = jest.fn(() => {
+      return Promise.reject(new Error());
+    });
+    const promise = sut.set(faker.database.column(), faker.random.word());
+    await expect(promise).rejects.toThrow(new StorageSetError());
   });
 });
