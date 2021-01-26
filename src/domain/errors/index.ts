fb73ff661df2acfa75bc02c1cabc4daac233acad
Robert Kelly <robertkelly1203@gmail.com>
2021-01-26 14:23:01 -0300

    refactor: add exports for storage domain errors

--- src/domain/errors/index.ts
+++ src/domain/errors/index.ts
@@ -1,2 +1,5 @@
 export * from './invalid-credentials-error';
+export * from './storage-clear-error';
+export * from './storage-get-error';
+export * from './storage-set-error';
 export * from './unexpected-error';
