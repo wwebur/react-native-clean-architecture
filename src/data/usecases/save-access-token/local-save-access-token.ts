e2332081afa25e4fc9426f4b153fc2124e4fa200
Robert Kelly <robertkelly1203@gmail.com>
2021-01-26 12:10:12 -0300

    refactor: rename LocalSaveAccessToken to LocalAccessTokenHandler

--- src/data/usecases/save-access-token/local-save-access-token.ts
+++
@@ -1,9 +0,0 @@
-import {Storage} from '@/data/protocols/storage/storage';
-import {SaveAccessToken} from '@/domain/usecases';
-
-export class LocalSaveAccessToken implements SaveAccessToken {
-  constructor(private readonly storage: Storage) {}
-  async save(accessToken: string): Promise<void> {
-    await this.storage.set('accessToken', accessToken);
-  }
-}
