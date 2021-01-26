df25d90ccc56550f6b5c727770693e802ac1e082
Robert Kelly <robertkelly1203@gmail.com>
2021-01-26 12:48:34 -0300

    refactor: move sut creation to a factory method

--- src/infra/storage/local-storage-adapter.spec.ts
+++ src/infra/storage/local-storage-adapter.spec.ts
@@ -2,12 +2,14 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import faker from 'faker';
 import {LocalStorageAdapter} from './local-storage-adapter';
 
+const makeSut = (): LocalStorageAdapter => new LocalStorageAdapter();
+
 describe('LocalStorageAdapter', () => {
   beforeEach(() => {
     AsyncStorage.clear();
   });
   test('Should call AsyncStorage with correct values', async () => {
-    const sut = new LocalStorageAdapter();
+    const sut = makeSut();
     const key = faker.database.column();
     const value = faker.random.word();
     await sut.set(key, value);
