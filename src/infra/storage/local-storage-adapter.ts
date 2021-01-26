e729a13e52d1b5bb62ffac4deabc0a5289993d82
Robert Kelly <robertkelly1203@gmail.com>
2021-01-26 13:16:48 -0300

    feat: ensure LocalStorageAdapter throws StorageSetError when AsyncStorage throws Error on setItem

--- src/infra/storage/local-storage-adapter.ts
+++ src/infra/storage/local-storage-adapter.ts
@@ -1,9 +1,14 @@
 import {Storage} from '@/data/protocols/storage/storage';
+import {StorageSetError} from '@/domain/errors/storage-set-error';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export class LocalStorageAdapter implements Storage<string> {
   async set(key: string, value: string): Promise<void> {
-    await AsyncStorage.setItem(key, value);
+    try {
+      await AsyncStorage.setItem(key, value);
+    } catch (error) {
+      throw new StorageSetError();
+    }
   }
   async get(key: string): Promise<string> {
     console.log(key);
