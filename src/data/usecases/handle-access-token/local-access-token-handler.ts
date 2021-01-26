bf2f36ac4a724a6d356b950a6a8e5f28e483fe93
Robert Kelly <robertkelly1203@gmail.com>
2021-01-26 12:59:16 -0300

    refactor: remove error handling from LocalAccessTokenHandler

--- src/data/usecases/handle-access-token/local-access-token-handler.ts
+++ src/data/usecases/handle-access-token/local-access-token-handler.ts
@@ -1,32 +1,17 @@
 import {Storage} from '@/data/protocols/storage/storage';
-import {StorageClearError} from '@/domain/errors/storage-clear-error';
-import {StorageGetError} from '@/domain/errors/storage-get-error';
-import {StorageSetError} from '@/domain/errors/storage-set-error';
 import {HandleAccessToken} from '@/domain/usecases';
 
 export class LocalAccessTokenHandler implements HandleAccessToken {
   constructor(private readonly storage: Storage<string>) {}
   async save(accessToken: string): Promise<void> {
-    try {
-      await this.storage.set('accessToken', accessToken);
-    } catch (error) {
-      throw new StorageSetError();
-    }
+    await this.storage.set('accessToken', accessToken);
   }
   async load(): Promise<string> {
-    try {
-      const value = await this.storage.get('accessToken');
-      return value;
-    } catch (error) {
-      throw new StorageGetError();
-    }
+    const value = await this.storage.get('accessToken');
+    return value;
   }
   async erase(): Promise<void> {
-    try {
-      const value = await this.storage.clear('accessToken');
-      return value;
-    } catch (error) {
-      throw new StorageClearError();
-    }
+    const value = await this.storage.clear('accessToken');
+    return value;
   }
 }
