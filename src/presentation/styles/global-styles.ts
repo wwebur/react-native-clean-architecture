6497960c1b41bdf7d977f1d08dfd8133768433a8
Robert Kelly <robertkelly1203@gmail.com>
2021-01-29 12:50:36 -0300

    feat: add global styles

---
+++ src/presentation/styles/global-styles.ts
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+
+export const GlobalStyles = StyleSheet.create({
+  bottomSafeArea: {flex: 1},
+  topSafeArea: {flex: 0},
+  container: {flex: 1},
+  content: {padding: 30},
+});
