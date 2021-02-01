ad0625124ccdff6e2389324caa09a44244390e5e
Robert Kelly <robertkelly1203@gmail.com>
2021-02-01 03:50:17 -0300

    test: ensure MinLengthValidation returns null if string is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -6,11 +6,19 @@ const makeSut = (fieldName: string, minLength: number): MinLengthValidation =>
   new MinLengthValidation(fieldName, minLength);
 
 describe('MinLengthValidation', () => {
-  test('Should return MinLengthError if string is under the required length', () => {
+  test('Should return MinLengthError if string length is under the required length', () => {
     const requiredLength = 5;
     const fieldName = faker.database.column();
     const sut = makeSut(fieldName, requiredLength);
     const error = sut.validate(faker.lorem.word(3));
     expect(error).toEqual(new MinLengthError(fieldName, requiredLength));
+  });
+
+  test('Should return null if string is valid', () => {
+    const requiredLength = 5;
+    const fieldName = faker.database.column();
+    const sut = makeSut(fieldName, requiredLength);
+    const error = sut.validate(faker.lorem.word(5));
+    expect(error).toBeNull();
   });
 });
