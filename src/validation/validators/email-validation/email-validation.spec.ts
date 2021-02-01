6b5234c52ba81e0fb7d2b4919bc6fc00c792c4d9
Robert Kelly <robertkelly1203@gmail.com>
2021-02-01 01:58:11 -0300

    test: should return null if email is valid

--- src/validation/validators/email-validation/email-validation.spec.ts
+++ src/validation/validators/email-validation/email-validation.spec.ts
@@ -1,10 +1,17 @@
 import {InvalidFieldError} from '@/validation/errors/invalid-field-error';
+import faker from 'faker';
 import {EmailValidation} from './email-validation';
 
 describe('EmailValidation', () => {
   test('Should return InvalidEmailError if email is invalid', () => {
-    const sut = new EmailValidation('email');
-    const error = sut.validate('');
+    const sut = new EmailValidation(faker.random.word());
+    const error = sut.validate(faker.random.word());
     expect(error).toEqual(new InvalidFieldError('email'));
+  });
+
+  test('Should return null if email if valid', () => {
+    const sut = new EmailValidation(faker.random.word());
+    const error = sut.validate(faker.internet.email());
+    expect(error).toBeNull();
   });
 });
