b8b22b545b0d5bee2cb035f8231e541bf620c326
Robert Kelly <robertkelly1203@gmail.com>
2021-02-01 03:59:02 -0300

    feat: ensure EmailValidation returns null if email is empty

--- src/validation/validators/email-validation/email-validation.ts
+++ src/validation/validators/email-validation/email-validation.ts
@@ -4,7 +4,10 @@ import {FieldValidation} from '@/validation/protocols';
 export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) {}
   validate(value: string): Error {
-    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return emailRegex.test(value) ? null : new InvalidFieldError('email');
+    if (value) {
+      const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return emailRegex.test(value) ? null : new InvalidFieldError('email');
+    }
+    return null;
   }
 }
