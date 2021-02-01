c068ea3ed8f7e4119d91dabfd17b382fdef96070
Robert Kelly <robertkelly1203@gmail.com>
2021-02-01 16:31:53 -0300

    test: ensure ValidationBuilder implements email method

--- src/validation/builder/validation-builder.spec.ts
+++ src/validation/builder/validation-builder.spec.ts
@@ -1,9 +1,14 @@
-import {RequiredFieldValidation} from '../validators';
+import {EmailValidation, RequiredFieldValidation} from '../validators';
 import {ValidationBuilder as sut} from './validation-builder';
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
     const validations = sut.field('any_field').required().build();
     expect(validations).toEqual([new RequiredFieldValidation('any_field')]);
+  });
+
+  test('Should return EmailValidation', () => {
+    const validations = sut.field('any_field').email().build();
+    expect(validations).toEqual([new EmailValidation('any_field')]);
   });
 });
