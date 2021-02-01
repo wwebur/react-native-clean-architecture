3bbb0d76511fae57f2f9228722266b3c3c4c5206
Robert Kelly <robertkelly1203@gmail.com>
2021-02-01 01:57:53 -0300

    feat: should return null if email is valid

--- src/validation/validators/email-validation/email-validation.ts
+++ src/validation/validators/email-validation/email-validation.ts
@@ -4,7 +4,7 @@ import {FieldValidation} from '@/validation/protocols';
 export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) {}
   validate(value: string): Error {
-    console.log(value);
-    return new InvalidFieldError('email');
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(value) ? null : new InvalidFieldError('email');
   }
 }
