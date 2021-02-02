848b901ffb01f64075081509c248c97ec4c897ae
Robert Kelly <robertkelly1203@gmail.com>
2021-02-02 11:32:17 -0300

    test: create DisplayFeedbackMessage usecase in data layer and MessagePresenter protocol tests

---
+++ src/data/usecases/display-message/display-feedback-message.spec.ts
@@ -0,0 +1,42 @@
+import {MessagePresenterSpy} from '@/data/test';
+import {makeFakeMessage} from '@/domain/test';
+import faker from 'faker';
+import {DisplayFeedbackMessage} from './display-feedback-message';
+
+type SutTypes = {
+  sut: DisplayFeedbackMessage;
+  messagePresenterSpy: MessagePresenterSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const messagePresenterSpy = new MessagePresenterSpy();
+  const sut = new DisplayFeedbackMessage(messagePresenterSpy);
+  return {sut, messagePresenterSpy};
+};
+
+describe('DisplayFeedbackMessage', () => {
+  test('Should call MessagePresenter with correct values on set', () => {
+    const {sut, messagePresenterSpy} = makeSut();
+    const fakeMessage = makeFakeMessage();
+    sut.show({title: fakeMessage.title, description: fakeMessage.description});
+    expect(messagePresenterSpy.message.title).toBe(fakeMessage.title);
+    expect(messagePresenterSpy.message.description).toBe(
+      fakeMessage.description,
+    );
+  });
+
+  test('Should throw error if MessagePresenter throws error', () => {
+    const {sut, messagePresenterSpy} = makeSut();
+    const errorMessage = faker.random.words();
+    const fakeMessage = makeFakeMessage();
+    jest
+      .spyOn(messagePresenterSpy, 'show')
+      .mockRejectedValueOnce(new Error(errorMessage));
+
+    const promise = sut.show({
+      title: fakeMessage.title,
+      description: fakeMessage.description,
+    });
+    expect(promise).rejects.toThrow(new Error(errorMessage));
+  });
+});
